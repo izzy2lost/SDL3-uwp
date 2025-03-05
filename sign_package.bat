e1bc12559d43dd899b57a2a38c8b061bad31ed56
izzy2lost <izzynochill@gmail.com>
2025-03-05 15:03:39 -0500

    Add files via upload

---
+++ sign_package.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the PackageRe-Sign folder
+cd /d "%~dp0PackageRe-Sign (Release)"
+
+REM Execute pkgrsn with the necessary arguments
+pkgrsn -a "%~dp0sdl-min_1.0.0.0_x64.msix" -o "%~dp0output" -p "CN=Ravbug" -x "auto" -s "Skip" -k
+
+REM Pause to keep the window open for review
+pause
