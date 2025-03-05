317a9202410496c121f3a5b9a844a2a179756877
izzy2lost <izzynochill@gmail.com>
2025-03-05 15:11:46 -0500

    Update sign_package.bat

--- sign_package.bat
+++ sign_package.bat
@@ -3,7 +3,7 @@ REM Navigate to the PackageRe-Sign folder
 cd /d "%~dp0PackageRe-Sign (Release)"
 
 REM Execute pkgrsn with the necessary arguments
-pkgrsn -a "%~dp0sdl-min_1.0.0.0_x64.msix" -o "%~dp0output" -p "CN=Ravbug" -x "auto" -s "Skip" -k
+pkgrsn.exe -a "%~dp0sdl-min_1.0.0.0_x64.msix" -o "%~dp0output" -p "CN=Ravbug" -x "auto" -s "Skip" -k
 
 REM Pause to keep the window open for review
 pause
