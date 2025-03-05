6f081e47692c0182c74ecd1328aee361d69574b4
izzy2lost <izzynochill@gmail.com>
2025-03-05 15:17:00 -0500

    Update sign_package.bat

--- sign_package.bat
+++ sign_package.bat
@@ -1,9 +1,15 @@
 @echo off
-REM Navigate to the PackageRe-Sign folder
-cd /d "%~dp0PackageRe-Sign (Release)"
+setlocal enabledelayedexpansion
 
-REM Execute pkgrsn with the necessary arguments
-pkgrsn.exe -a "%~dp0sdl-min_1.0.0.0_x64.msix" -o "%~dp0output" -p "CN=Ravbug" -x "auto" -s "Skip" -k
+:: Set the directory where pkgrsn.exe is located
+set PKGRSN_DIR=%~dp0
 
-REM Pause to keep the window open for review
-pause
+:: Set the MSIX file and output folder
+set MSIX_FILE=%~dp0..\sdl-min_1.0.0.0_x64.msix
+set OUTPUT_DIR=%~dp0..\output
+
+:: Ensure output directory exists
+if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+
+:: Run pkgrsn.exe with appropriate arguments
+"%PKGRSN_DIR%\pkgrsn.exe" -a "%MSIX_FILE%" -o "%OUTPUT_DIR%" -p "CN=Ravbug" --skip
